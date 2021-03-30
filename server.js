953a8bf52ca78e67383b61c9fe1457c7f622dae5
Julio Torres <julio.torres@e2ycommerce.com>
2021-03-30 13:10:03 +0200

    Clean up your project with modules

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const passport = require('passport');
 const session = require('express-session');
-const ObjectID = require('mongodb').ObjectID;
+const routes = require('./routes');
+const auth = require('./auth');
 
 const app = express();
 app.set('view engine', 'pug')
@@ -13,51 +14,30 @@ app.set('view engine', 'pug')
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'))
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: true,
-  saveUninitialized: true,
-  cookie: { secure: false }
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    cookie: {secure: false}
 }));
 app.use(passport.initialize());
 app.use(passport.session());
-app.route('/').get((req, res) => {
-  res.render('pug',{title: 'Hello', message: 'Please login'});
-});
 
 myDB(async client => {
-  const myDataBase = await client.db('database').collection('users');
+    const myDataBase = await client.db('database').collection('users');
+    routes(app, myDataBase);
+    auth(app, myDataBase);
 
-  // Be sure to change the title
-  app.route('/').get((req, res) => {
-    //Change the response to render the Pug template
-    res.render('pug', {
-      title: 'Connected to Database',
-      message: 'Please login'
-    });
-  });
 
-  // Serialization and deserialization here...
-  passport.serializeUser((user, done) => {
-    done(null, user._id);
-  });
-
-  passport.deserializeUser((id, done) => {
-    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-      done(null, doc);
-    });
-  });
-
-  // Be sure to add this...
 }).catch(e => {
-  app.route('/').get((req, res) => {
-    res.render('pug', { title: e, message: 'Unable to login' });
-  });
+    app.route('/').get((req, res) => {
+        res.render('pug', {title: e, message: 'Unable to login'});
+    });
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log('Listening on port ' + PORT);
+    console.log('Listening on port ' + PORT);
 });
 
